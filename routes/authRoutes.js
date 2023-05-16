f81eb05754704a8c10008e550d539b61f75de8c7
Dylan Grissom <dgriss295@gmail.com>
2023-05-15 20:35:37 -0500

    first push of authentications

---
+++ routes/authRoutes.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+const router = express.Router();
+
+// Register route
+router.post('/register', (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Check if the user already exists
+  User.findOne({ where: { email: email } })
+    .then((existingUser) => {
+      if (existingUser) {
+        return res.status(400).json({ message: 'Email is already registered' });
+      }
+
+      // Create a new user
+      bcrypt.hash(password, 10, (err, hashedPassword) => {
+        if (err) {
+          return res.status(500).json({ message: 'Error hashing password' });
+        }
+
+        User.create({ name, email, password: hashedPassword })
+          .then((user) => {
+            req.login(user, (err) => {
+              if (err) {
+                return res.status(500).json({ message: 'Error logging in' });
+              }
+              res.json({ user });
+            });
+          })
+          .catch((err) => {
+            res.status(500).json({ message: 'Error creating user' });
+          });
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Error finding user' });
+    });
+});
+
+// Login route
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  // Authentication successful, handle the response as needed
+  res.json({ user: req.user });
+});
+
+// Logout route
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.json({ message: 'Logout successful' });
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/note', require('./note'));
+// Import routes
+const noteRoutes = require('./note');
+const authRoutes = require('./authRoutes');
+
+// Middleware for note routes
+router.use('/note', noteRoutes);
+
+// Middleware for authentication routes
+router.use('/auth', authRoutes);
 
 module.exports = router;
