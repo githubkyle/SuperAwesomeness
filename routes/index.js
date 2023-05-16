98646dcf4abe81a0b1f9063239d8a6e1cf78b139
Aubrey Gunn <agunn369@gmail.com>
2023-05-15 21:05:27 -0500

    handlebars

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const noteRoutes = require('./note');
 
 router.use('/note', require('./note'));
+router.use('/', noteRoutes)
 
 module.exports = router;
