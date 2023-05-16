cfc20ffecccad002d2cea168ac847a22930339df
kshwi <kshwiss@gmail.com>
2023-05-15 20:49:43 -0500

    added to routes, polishing

--- routes/note.js
+++ routes/note.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const noteController = require("../controllers/noteController");
-const { Note, Tag } = require("../app/models");
+const { Note, Tag, User } = require("../app/models");
+const connection = require("./config/database");
 
 // GET all notes
 router.get("/", (req, res) => {
@@ -10,7 +11,7 @@ router.get("/", (req, res) => {
     include: [
       {
         model: Tag,
-        attributes: ["id", "tag_name"],
+        attributes: ["tag_name"],
         include: {
           model: User,
           attributes: ["username"]
@@ -20,16 +21,39 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/note/:id", (req, res) => {
+  var N = Note.findByPk(req);
+  res = N;
+});
+
+router.post("/note", (req, res) => {
+  Note.create({ note_text: req });
+  if (err) throw err;
+  res = console.log("Successfully created note");
+});
+
+router.put("/note/:id", (req, res) => {
+  Note.update({ note_text: req });
+  if (err) throw err;
+  res = console.log("Successfully updated that note");
+});
+
+router.destroy("/note/:id", (req, res) => {
+  Note.destroy(req);
+  if (err) throw err;
+  res = console.log("Successfully nuked that note");
+});
+
 // GET a single note by ID
-router.get("/:id", noteController.getNoteById);
+// router.get("/:id", noteController.getNoteById);
 
-// POST a new note
-router.post("/", noteController.createNote);
+// // POST a new note
+// router.post("/", noteController.createNote);
 
-// PUT (update) an existing note
-router.put("/:id", noteController.updateNote);
+// // PUT (update) an existing note
+// router.put("/:id", noteController.updateNote);
 
-// DELETE a note
-router.delete("/:id", noteController.deleteNote);
+// // DELETE a note
+// router.delete("/:id", noteController.deleteNote);
 
-module.exports = router;
+// module.exports = router;
