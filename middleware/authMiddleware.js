755c5366bdf49624baa63830a2c48411b7e5a964
Dylan Grissom <dgriss295@gmail.com>
2023-05-17 19:11:09 -0500

    added authmiddleware and config.json files

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,12 @@
+// Middleware function to check if the user is authenticated
+const ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      // If the user is authenticated, proceed to the next middleware/route handler
+      return next();
+    }
+    // If the user is not authenticated, redirect them to the login page
+    res.redirect('/auth/login');
+  };
+  
+  module.exports = { ensureAuthenticated };
+  
