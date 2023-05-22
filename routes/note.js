71294ca56a7fbdb997f15c3f692bbb64cf95c310
kshwi <kshwiss@gmail.com>
2023-05-22 12:48:06 -0500

    adjusting the post note route

--- routes/note.js
+++ routes/note.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { Note } = require("../app/models");
+const bodyParserErrorHandler = require("express-body-parser-error-handler");
+const { urlencoded, json } = require("body-parser");
+const app = express();
+var bodyParser = require("body-parser");
 
 router.get("/", async (req, res) => {
   try {
@@ -36,7 +40,10 @@ router.get("/note/:id", async (req, res) => {
 router.post("/note", async (req, res) => {
   try {
     const note = await Note.create({
-      note_text: req.body.note_text
+      note_title: req.body.note_title,
+      note_text: req.body.note_text,
+      note_tag: req.body.note_tag,
+      note_gif: req.body.note_gif
     });
     res.status(201).json({ message: "Successfully created note", note });
   } catch (err) {
@@ -50,7 +57,10 @@ router.put("/note/:id", async (req, res) => {
     const noteId = req.params.id;
     const updatedNote = await Note.update(
       {
-        note_text: req.body.note_text
+        note_title: req.body.note_title,
+        note_text: req.body.note_text,
+        note_tag: req.body.note_tag,
+        note_gif: req.body.note_gif
       },
       {
         where: {
@@ -81,5 +91,17 @@ router.delete("/note/:id", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.use(bodyParser.json());
+
+// body parser error handler
+app.use(bodyParserErrorHandler());
+app.use(router);
 
 module.exports = router;
