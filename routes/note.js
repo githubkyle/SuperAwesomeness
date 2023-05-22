dd27cf5d87a61d21d0c3bd1dba4d03c573d734de
kshwi <kshwiss@gmail.com>
2023-05-21 23:05:53 -0500

    cleaned up some commented code

--- routes/note.js
+++ routes/note.js
@@ -1,30 +1,7 @@
 const express = require("express");
 const router = express.Router();
-// const noteController = require("../controllers/noteController");
-const { Note, Tag, User } = require("../app/models");
-// const connection = require("./config/database");
 
-// GET all notes
-// router.get("/", async (req, res) => {
-//   try {
-//     var NPO = await Note.findAll({
-//       attributes: ["id", "note_text"],
-//       include: [
-//         {
-//           model: Tag,
-//           attributes: ["tag_name"],
-//           include: {
-//             model: User,
-//             attributes: ["username"]
-//           }
-//         }
-//       ]
-//     });
-//     res.status(200).json(NPO);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+const { Note, Tag, User } = require("../app/models");
 
 router.get("/", async (req, res) => {
   try {
@@ -68,17 +45,5 @@ router.delete("/note/:id", (req, res) => {
   if (err) throw err;
   res = console.log("Successfully nuked that note");
 });
-
-// GET a single note by ID
-// router.get("/:id", noteController.getNoteById);
-
-// // POST a new note
-// router.post("/", noteController.createNote);
-
-// // PUT (update) an existing note
-// router.put("/:id", noteController.updateNote);
-
-// // DELETE a note
-// router.delete("/:id", noteController.deleteNote);
 
 module.exports = router;
